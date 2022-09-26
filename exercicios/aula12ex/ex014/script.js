ff2ec378bc7812c002eb4826a2796525fe6f956a
Gilberto Ribeiro <111013759+ribeirogilberto@users.noreply.github.com>
2022-09-26 12:46:37 -0300

    alteração no formato da imagem
    de jpg para png

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -6,15 +6,15 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
-        img.src = 'fotomanha.jpg'
+        img.src = 'fotomanha.png'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
         // BOA TARDE!
-        img.src = 'fototarde.jpg'
+        img.src = 'fototarde.png'
         document.body.style.background = '#b9846f'
     } else {
         // BOA NOITE!
-        img.src = 'fotonoite.jpg'
+        img.src = 'fotonoite.png'
         document.body.style.background = '#515154'
     }
 }
