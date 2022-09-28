1ac672ec67e40dda355f5a3597860a101e4a62b3
Gilberto Ribeiro <111013759+ribeirogilberto@users.noreply.github.com>
2022-09-27 23:00:26 -0300

    Update script.js

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -10,12 +10,40 @@ function verificar() {
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
-        if (fsex[0.checked]) {
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
         }
-        res.getElementsByClassName.textAlign = 'center'
-        res.innerHTMLL = `Detectamos ${gênero} com ${idade} anos.`
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
