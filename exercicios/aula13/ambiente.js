e573f61505456fb0a4d6e38e89506f2ee90dd92b
Gilberto Ribeiro <111013759+ribeirogilberto@users.noreply.github.com>
2022-09-29 21:23:13 -0300

    aula13 -  Repetições (Parte 1)
    Estrutura de Repetição com Teste Lógico no Início.
    Estrutura de Repetição com Teste Lógico no Final.

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,39 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+/*
+
+ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO (mais utilizada)
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+*/
+
+
+/*
+ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+*/
+
+
+/*
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+*/
