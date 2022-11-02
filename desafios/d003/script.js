a2db8813a25c3e18d4b304140983703e61d1ad8a
Gilberto Ribeiro <111013759+ribeirogilberto@users.noreply.github.com>
2022-11-02 10:36:23 -0300

    Antecessor e Sucessor
    Desafio 03

---
+++ desafios/d003/script.js
@@ -0,0 +1,5 @@
+function d003() {
+    let number = Number(prompt('Digite um número qualquer'))
+
+alert(`Antes de ${number}, temos o número ${number-1}. Depois de ${number}, temos o número ${number+1}.`)
+}
