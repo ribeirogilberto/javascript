31ecaa0468c530f5bd69eafd54610b2c3ad8a32a
Gilberto Ribeiro <111013759+ribeirogilberto@users.noreply.github.com>
2023-01-19 00:14:36 -0300

    Exercícios JavaScipt (Parte 5)
    Super Contador

--- aulas/aula14ex/ex016/script.js
+++ aulas/aula14ex/ex016/script.js
@@ -5,8 +5,8 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossivel contar!'
-        //window.alert('[ERRO] faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
         res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
@@ -20,13 +20,13 @@ function contar() {
             // Contagem crescente
             for(let c = i; c <= f; c += p) {
                 res.innerHTML += ` ${c} \u{1F449}`
-            }  
+            }
         } else {
             // Contagem regressiva
             for(let c = i; c >= f; c -= p) {
                 res.innerHTML += ` ${c} \u{1F449}`
             }
         }
-          res.innerHTML += `\u{1F3C1}`
+        res.innerHTML += `\u{1F3C1}`
     }
 }
