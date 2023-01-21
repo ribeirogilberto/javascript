b21539932794ebc1fd04ed545d7acce9f963dc43
Gilberto Ribeiro <111013759+ribeirogilberto@users.noreply.github.com>
2023-01-20 23:53:33 -0300

    Usando um JavaScript externo
    Exercício 10

---
+++ exercicios/ex010/script.js
@@ -0,0 +1,17 @@
+let resp = document.getElementById('saida')
+
+function ação1() {
+    resp.innerHTML +=  `<p>Você clicou no primeiro botão</p>`
+}
+
+function ação2() {
+    resp.innerHTML += `<p>Você clicou no segundo botão</p>`
+}
+
+function ação3() {
+    resp.innerHTML += `<p>Você clicou no terceiro botão</p>`
+}
+
+function ação4() {
+    resp.innerHTML += `<p>Você clicou no quarto botão</p>`
+}
