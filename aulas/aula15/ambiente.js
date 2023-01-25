bbe6477866f09ceed082ceb225117dc9d2adfeb1
Gilberto Ribeiro <111013759+ribeirogilberto@users.noreply.github.com>
2023-01-24 22:23:15 -0300

    Variáveis Compostas
    Aula 15

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -1,10 +1,10 @@
-let num = [5, 8, 2, 9, 3]
-num.push(1)
-num.sort()
+let num = [5, 8, 2, 9, 3] // Variáveis Compostas devem ser capazes de armazenar vários valores em uma mesma estrutura
+num.push(1) // Caso você queira adicionar um número no final, independente de onde o final seja.
+num.sort() // Coloca de forma ordenada, ou seja, em ordem crescente
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(9)
+let pos = num.indexOf(9) // indeOf() Pesquisa dentro do vetor em qual posição se encontra a chave/indice
 if (pos == -1) {
     console.log('O valor não foi encontrado!')
 } else {
